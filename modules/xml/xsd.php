1f5a138035b9a7ace28784649a80513315ef31cb
Gaetano Giunta <gg@ez.no>
2008-03-06 11:33:40 +0000

    - Initial check in of code
    git-svn-id: http://svn.projects.ez.no/ggxmlview@1 f0645672-6e5d-4764-8af2-a54dcdef305a

---
+++ modules/xml/xsd.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Generates an xsd file for all the content classes present in this eZ Publish instance
+ *
+ * @version $Id: $
+ * @author Gaetano Giunta
+ * @copyright (c) 2008 G. Giunta
+ * @license code licensed under the GPL License: see README
+ */
+
+/// fecth all classes definitions
+$classes = eZContentClass::fetchAllClasses();
+
+/// feed them to the template
+$tpl = templateInit();
+$tpl->setVariable( "classes", $classes );
+
+$Result = array();
+$Result['pagelayout'] = 'xml_pagelayout.tpl';
+$Result['content'] = $tpl->fetch( "design:class/xml_classlist.tpl" );
+
+header('Content-type: text/xml');
+
+?>
---
+++ settings/layout.ini.append.php
@@ -0,0 +1,12 @@
+<?php /*
+
+# set up a new pagelayout to be used by the layout module
+[xml]
+PageLayout=xml_pagelayout.tpl
+ContentType=text/xml
+
+[json]
+PageLayout=json_pagelayout.tpl
+#ContentType=application/json
+
+*/ ?>
