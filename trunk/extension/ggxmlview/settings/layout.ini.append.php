188b3cc32f29219fc271df559b416fd0a7582782
Gaetano Giunta <gg@ez.no>
2008-03-08 15:59:57 +0000

    - Fix: remove path of layout module from generated urls
    git-svn-id: http://svn.projects.ez.no/ggxmlview@11 f0645672-6e5d-4764-8af2-a54dcdef305a

--- trunk/extension/ggxmlview/settings/layout.ini.append.php
+++ trunk/extension/ggxmlview/settings/layout.ini.append.php
@@ -4,9 +4,11 @@
 [xml]
 PageLayout=xml_pagelayout.tpl
 ContentType=text/xml
+UseAccessPass=false
 
 [json]
 PageLayout=json_pagelayout.tpl
-#ContentType=application/json
+ContentType=application/json
+UseAccessPass=false
 
 */ ?>
